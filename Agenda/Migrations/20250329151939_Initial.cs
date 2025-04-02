63876774b6e6ab23af27bf3e7b4fd025f8f10264
office <office@SICAP-PC2>
2025-04-02 08:56:52 +0300

    Add project files.

---
+++ Agenda/Migrations/20250329151939_Initial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agenda.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Utilizator",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nume = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prenume = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefon = table.Column<string>(type: "int", nullable: true),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Observatie = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilizator", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Utilizator");
+        }
+    }
+}
---
+++ Agenda/Models/Utilizator.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Models
+{
+    public class Utilizator
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Numele este necesar.")]
+        public string? Nume { get; set; }
+        public string? Prenume { get; set; }
+
+        [RegularExpression(@"^\+40[0-9]{9}$", ErrorMessage = "Numarul de telefon trebuie sa aiba formatul +40 urmat de 9 cifre.")]
+        public string? Telefon { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? Observatie { get; set; }
+    }
+}
